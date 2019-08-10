675697e7abd40e16b09700a3d21213cb2afd4b75
Jake Seever <jseever@my365.bellevue.edu>
2019-08-10 15:03:46 -0500

    Adding the 5.3 exercise files.

---
+++ week-5/seever-md-login/src/app/app.module.ts
@@ -0,0 +1,35 @@
+/*
+============================================
+; Title:  app.module.ts
+; Author: Jake Seever
+; Date:   10 August 2019
+; Description: md login Angular Materials example
+;===========================================
+*/
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {MatButtonModule, MatCardModule, MatFormFieldModule, MatInputModule, MatToolbarModule} from "@angular/material";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { FormsModule } from '@angular/forms'
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ week-5/seever-md-login/src/main.ts
@@ -0,0 +1,21 @@
+/*
+============================================
+; Title:  main.ts
+; Author: Jake Seever
+; Date:   10 August 2019
+; Description: md login Angular Materials example
+;===========================================
+*/
+
+import { enableProdMode } from '@angular/core';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from './app/app.module';
+import { environment } from './environments/environment';
+
+if (environment.production) {
+  enableProdMode();
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule)
+  .catch(err => console.error(err));
