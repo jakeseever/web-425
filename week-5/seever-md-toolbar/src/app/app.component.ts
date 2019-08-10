4de19c984362b8ae64b280ee74f3277771aebb05
Jake Seever <jseever@my365.bellevue.edu>
2019-08-10 14:22:33 -0500

    Adding the 3.2 exercise files.

---
+++ week-5/seever-md-toolbar/src/app/app.component.ts
@@ -0,0 +1,19 @@
+/*
+============================================
+; Title:  app.component.ts
+; Author: Jake Seever
+; Date:   10 August 2019
+; Description: md toolbar Angular Materials example
+;===========================================
+*/
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'seever-md-toolbar';
+}
---
+++ week-5/seever-md-toolbar/src/app/app.module.ts
@@ -0,0 +1,34 @@
+/*
+============================================
+; Title:  app.module.ts
+; Author: Jake Seever
+; Date:   10 August 2019
+; Description: md toolbar Angular Materials example
+;===========================================
+*/
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import {MatButtonModule, MatIconModule, MatMenuModule, MatToolbarModule} from "@angular/material";
+import {FlexLayoutModule} from "@angular/flex-layout";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatToolbarModule,
+    FlexLayoutModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
